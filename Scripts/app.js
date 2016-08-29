aedefee529dcdc776c6fae817fb05ee2147b0d3e
Kevin Ma <kbma.kevin@gmail.com>
2016-08-29 19:54:01 -0400

    removed jqueryui

--- Scripts/app.js
+++ Scripts/app.js
@@ -12,57 +12,10 @@
 (function () {
     "use strict";
 
-    function readAddressBook() {
-        $.getJSON("contacts.json", (data) => {
-            var addressbook = data;
-            var contacts = addressbook.contacts;
-
-            contacts.forEach(function (contact) {
-                console.log(contact);
-            }, this);
-        })
-    }
-
-    function setActivePage() {
-        switch (document.title) {
-            case "Home":
-                document.getElementById("index").setAttribute("class", "active");
-                break;
-            case "About Me":
-                document.getElementById("about").setAttribute("class", "active");
-                break;
-            case "Projects":
-                document.getElementById("projects").setAttribute("class", "active");
-                break;
-            case "Contact Me":
-                document.getElementById("contact").setAttribute("class", "active");
-                break;
-        }
-    }
-
-    function loadFooter() {
-        $('#footer').load('Partials/footer.html', () => {
-            $('#year').text(new Date().getFullYear())
-        })
-    }
-
-    function loadNavBar() {
-        $('#mainNav').load('Partials/navbar.html', () => {
-            $('#projects').replaceWith('<li id= "gallery" > <a href="gallery.html"><i class="fa fa-picture-o fa-lg"></i> Gallery</a></li>')
-            setActivePage()
-        })
-
-    }
-
     // app entry function
     function init() {
-        loadNavBar();
 
-        readAddressBook();
 
-        loadFooter();
-
-        
     }
 
 
